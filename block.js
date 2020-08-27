0916ac819fb76179e0868a5a62e60a451e554bb6
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-27 15:04:51 +0530

    added hash function

--- block.js
+++ block.js
@@ -1,3 +1,4 @@
+var SHA256 = require("crypto-js/sha256");
 class Block {
   constructor(timestamp, lastHash, hash, data) {
     this.timestamp = timestamp;
@@ -22,9 +23,13 @@ class Block {
   static mineBlock(lastBlock, data) {
     const timestamp = Date.now();
     const lastHash = lastBlock.hash;
-    const hash = "todo-hash";
+    const hash = Block.hash(timestamp, lastHash, data);
 
     return new this(timestamp, lastHash, hash, data);
+  }
+
+  static hash(timestamp, lastHash, data) {
+    return SHA256("${timestamp}${lastHash}${data}").toString();
   }
 }
 
