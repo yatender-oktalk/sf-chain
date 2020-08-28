6f3aac028367f6d3642ca2d5ba3adf6302420644
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-28 12:15:57 +0530

    setting up the ws

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const Blockchain = require("../blockchain");
 const bodyParser = require("body-parser");
+const P2pServer = require("./p2p-server");
+
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
@@ -21,3 +24,4 @@ app.post("/mine", (req, res) => {
 app.listen(HTTP_PORT, () => {
   console.log(`blockchain is running on ${HTTP_PORT}`);
 });
+p2pServer.listen();
