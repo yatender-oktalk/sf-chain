31df1dd54878a8fec9567714f3d5a723b531cf60
yatender-oktalk <yatender.nitk@gmail.com>
2020-08-30 16:31:06 +0530

    adding nonce

--- blockchain/block.js
+++ blockchain/block.js
@@ -5,6 +5,7 @@ class Block {
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+    this.nonce = nonce;
   }
 
   to_string() {
@@ -17,24 +18,28 @@ class Block {
   }
 
   static genesis() {
-    return new this("Genesis time", "----", "foo", []);
+    return new this("Genesis time", "----", "foo", [], 0);
   }
 
   static mineBlock(lastBlock, data) {
+    let hash, timestamp;
     const timestamp = Date.now();
     const lastHash = lastBlock.hash;
-    const hash = Block.hash(timestamp, lastHash, data);
+    let nonce = 0;
+    do {
+      const hash = Block.hash(timestamp, lastHash, data, nonce);
+    } while()
 
     return new this(timestamp, lastHash, hash, data);
   }
 
-  static hash(timestamp, lastHash, data) {
-    return SHA256(`${timestamp}${lastHash}${data}`).toString();
+  static hash(timestamp, lastHash, data, nonce) {
+    return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString();
   }
 
   static blockHash(block) {
-    const { timestamp, lastHash, data } = block;
-    return Block.hash(timestamp, lastHash, data);
+    const { timestamp, lastHash, data, nonce } = block;
+    return Block.hash(timestamp, lastHash, data, nonce);
   }
 }
 
