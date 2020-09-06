2f93c7e103d42eb606321140bb33e27638abe040
yatender-oktalk <yatender.nitk@gmail.com>
2020-09-06 16:40:47 +0530

    creating the wallet

---
+++ chain-util.js
@@ -0,0 +1,13 @@
+const { ec } = require("elliptic");
+
+const EC = require("elliptic").ec;
+cost ec = new EC('secp256k1');
+
+class ChainUtil {
+  static genKeyPair() {
+    return ec.genKeyPair();
+  }
+}
+
+
+module.exports = ChainUtil;
