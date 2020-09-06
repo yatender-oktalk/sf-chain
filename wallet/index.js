03264cd9d627cee286285b65856608cc0ad54522
yatender-oktalk <yatender.nitk@gmail.com>
2020-09-06 15:57:37 +0530

    creating wallet Class

---
+++ wallet/index.js
@@ -0,0 +1,16 @@
+const { INITIAL_BALANCE } = require("..config");
+
+class Wallet {
+  constructor() {
+    this.balance = INITIAL_BALANCE;
+    this.keyPair = null;
+    this.publicKey = null;
+  }
+
+  toString() {
+    return `Wallet -
+      publicKey: ${this.publicKey.toString()}
+      balance: ${this.balance}
+    `;
+  }
+}
