dfa528fc71ac121069d9f9b08e58d16f5f39c00d
yatender-oktalk <yatender.nitk@gmail.com>
2020-09-20 12:00:48 +0530

    Fixing the tests

--- transaction.test.js
+++ transaction.test.js
@@ -1,30 +1,46 @@
 const Transaction = require("./wallet/transaction");
-
 const Wallet = require("./wallet/index");
 
 describe("Transaction", () => {
   let transaction, wallet, recipient, amount;
-
   beforeEach(() => {
     wallet = new Wallet();
-    amount: 50;
-    recipient: "r3c1p13nt";
-    transaction: Transaction.newTransaction(wallet, recipient, amount);
+    amount = 50;
+    recipient = "r3c1p13nt";
+    transaction = Transaction.newTransaction(wallet, recipient, amount);
   });
 
-  it("outputs the `amount` subtracted from the balance", () => {
+  it("ouputs the `amount` subtracted from the wallet balance", () => {
     expect(
-      transaction.outputs
-        .find(transaction.output.address === wallet.publicKey)
-        .amount.toEqual(wallet.balance - amount)
-    );
+      transaction.outputs.find((output) => output.address === wallet.publicKey)
+        .amount
+    ).toEqual(wallet.balance - amount);
   });
 
   it("outputs the `amount` added to the recipient", () => {
     expect(
-      transaction.outputs
-        .find(transaction.output.address === recipient)
-        .amount.toEqual(amount)
-    );
+      transaction.outputs.find((output) => output.address === recipient).amount
+    ).toEqual(amount);
+  });
+
+  describe("transacting with an amount that exceeds the balance", () => {
+    beforeEach(() => {
+      amount = 50000;
+      transaction = Transaction.newTransaction(wallet, recipient, amount);
+    });
+
+    it("does not create the transaction", () => {
+      expect(transaction).toEqual(undefined);
+    });
   });
 });
+
+// Make sure to change the testing environment for Jest from the default mode to “node”. In package.json, add this rule:
+// ```
+// ...
+// "jest": {
+// "testEnvironment": "node"
+// },
+// ```
+
+// $ npm run test
