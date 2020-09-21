78204b1d7b560593fc837673ea56a7be7875221e
yatender-oktalk <yatender.nitk@gmail.com>
2020-09-21 10:13:00 +0530

    Adding the api for transactions and transact

--- app/index.js
+++ app/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const Blockchain = require("../blockchain");
 const bodyParser = require("body-parser");
 const P2pServer = require("./p2p-server");
+const Wallet = require("../wallet");
+const TransactionPool = require("../wallet/transaction-pool");
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
@@ -22,6 +26,16 @@ app.post("/mine", (req, res) => {
   p2pServer.syncChains();
 
   res.redirect("/blocks");
+});
+
+app.get("/transactions", (req, res) => {
+  res.json(tp.transactions);
+});
+
+app.post("/transact", (req, res) => {
+  const { recipient, amount } = req.body;
+  const transaction = wallet.createTransaction(recipient, amount, tp);
+  res.redirect("/transactions");
 });
 
 app.listen(HTTP_PORT, () => {
