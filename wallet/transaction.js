fb2ed904606e92f1a88efd4cd5134573e0d2e514
yatender-oktalk <yatender.nitk@gmail.com>
2020-09-21 12:46:23 +0530

    Adding the mining reward

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,5 +1,5 @@
 const ChainUtil = require("../chain-util");
-
+const { MINING_REWARD } = require("../config");
 class Transaction {
   constructor() {
     this.id = ChainUtil.id();
@@ -24,6 +24,13 @@ class Transaction {
     return this;
   }
 
+  static transactionWithOutputs(senderWallet, outputs) {
+    const transaction = new this();
+    transaction.outputs.push(...outputs);
+    Transaction.signTransaction(transaction, senderWallet);
+    return transaction;
+  }
+
   static newTransaction(senderWallet, recipient, amount) {
     const transaction = new this();
     if (amount > senderWallet.balance) {
@@ -31,19 +38,22 @@ class Transaction {
       return;
     }
 
-    transaction.outputs.push(
-      ...[
-        {
-          amount: senderWallet.balance - amount,
-          address: senderWallet.publicKey,
-        },
-        { amount, address: recipient },
-      ]
-    );
+    return Transaction.transactionWithOutputs(senderWallet, [
+      {
+        amount: senderWallet.balance - amount,
+        address: senderWallet.publicKey,
+      },
+      { amount, address: recipient },
+    ]);
+  }
 
-    Transaction.signTransaction(transaction, senderWallet);
-
-    return transaction;
+  static rewardTransaction(minerWallet, blockchainWallet) {
+    return Transaction.transactionWithOutputs(blockchainWallet, [
+      {
+        amount: MINING_REWARD,
+        address: minerWallet.publicKey,
+      },
+    ]);
   }
 
   static signTransaction(transaction, senderWallet) {
